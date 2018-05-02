5a2721822744e3c85a0be6970e9522c34c2c9c0c
anelcc <anel.calvo.cervantes@gmail.com>
2018-05-02 02:52:15 -0400

    create class and interface

---
+++ app/src/main/java/com/anelcalvo/www/dagger_2/di/Boltons.java
@@ -0,0 +1,21 @@
+package com.anelcalvo.www.dagger_2.di;
+
+import com.anelcalvo.www.dagger_2.di.House;
+
+/**
+ * Created by ctsuser1 on 5/2/18.
+ */
+
+public class Boltons implements House {
+    @Override
+    public void prepareForWar() {
+        //do something
+        System.out.println(this.getClass().getSimpleName()+" prepared for war");
+    }
+
+    @Override
+    public void reportForWar() {
+        //do something
+        System.out.println(this.getClass().getSimpleName()+" reporting..");
+    }
+}
---
+++ app/src/main/java/com/anelcalvo/www/dagger_2/di/House.java
@@ -0,0 +1,12 @@
+package com.anelcalvo.www.dagger_2.di;
+
+/**
+ * Created by anel.calvo on 5/2/18.
+ */
+
+public interface House {
+    void prepareForWar();
+
+    void reportForWar();
+
+}
